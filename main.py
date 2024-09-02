794914901c2fd82dd53dd5277a0d5a6e7f60e905
KSebasTino <72003459@continental.edu.pe>
2024-09-02 18:52:03 -0500

    Proyecto de suma en POO

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numero01=float(input("Ingrese sumando uno: "))
-numero02=float(input("Ingrese sumando dos: "))
-suma=numero01+numero02
-print(f"{numero01}+{numero02}={suma}")
+from OperacionesAritmeticas import OperacionesAritmeticas
+
+if __name__ == '__main__':
+    operacion = OperacionesAritmeticas
+    num1,num2 = operacion.ingresoNumeros()
+print(f'{num1}+{num2}={operacion.suma(num1, num2)}')
